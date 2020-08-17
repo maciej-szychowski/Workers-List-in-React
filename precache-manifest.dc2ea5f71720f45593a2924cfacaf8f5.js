4cd8818320e5dc765ccd0d25c7be5adc79372183
Macius <maciej85_maciej85@o2.pl>
2020-08-17 18:59:15 +0200

    Updates

---
+++ precache-manifest.dc2ea5f71720f45593a2924cfacaf8f5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb0a84f602cc86b69b714da4bdfea07",
+    "url": "/Workers-List-in-React/index.html"
+  },
+  {
+    "revision": "bc6921cfd90e687cc3cf",
+    "url": "/Workers-List-in-React/static/css/main.56771c66.chunk.css"
+  },
+  {
+    "revision": "98532fdf9702e3b77823",
+    "url": "/Workers-List-in-React/static/js/2.c157d5d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Workers-List-in-React/static/js/2.c157d5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6921cfd90e687cc3cf",
+    "url": "/Workers-List-in-React/static/js/main.bce0a13a.chunk.js"
+  },
+  {
+    "revision": "46105c79470f9263582b",
+    "url": "/Workers-List-in-React/static/js/runtime-main.88e237d7.js"
+  },
+  {
+    "revision": "925864839f5cd68d0e8cdd443d32b355",
+    "url": "/Workers-List-in-React/static/media/add.92586483.svg"
+  },
+  {
+    "revision": "e65ea0a6b3cca53f9b7ebccbafcda303",
+    "url": "/Workers-List-in-React/static/media/article.e65ea0a6.svg"
+  },
+  {
+    "revision": "6318d5b905dd23c0419db5e6ea04d325",
+    "url": "/Workers-List-in-React/static/media/avatar.6318d5b9.svg"
+  },
+  {
+    "revision": "2e86c766f79c4b7a7f55e771b0ddd5a9",
+    "url": "/Workers-List-in-React/static/media/brightness.2e86c766.svg"
+  },
+  {
+    "revision": "39e9328f0cd03803ea6aeafac1f66451",
+    "url": "/Workers-List-in-React/static/media/brightness2.39e9328f.svg"
+  },
+  {
+    "revision": "c38db5bfd1e56c3495ab26a66a2f2244",
+    "url": "/Workers-List-in-React/static/media/delete.c38db5bf.svg"
+  },
+  {
+    "revision": "a61b636a9adfc9d33e6133cbb8ae3516",
+    "url": "/Workers-List-in-React/static/media/edit-pen.a61b636a.svg"
+  },
+  {
+    "revision": "42305ce466180bc8d7ea8ce5bd1979c2",
+    "url": "/Workers-List-in-React/static/media/library.42305ce4.svg"
+  },
+  {
+    "revision": "40cf5acb0fef19645e0c4bdd17536ef3",
+    "url": "/Workers-List-in-React/static/media/logo.40cf5acb.svg"
+  },
+  {
+    "revision": "a57456668144b2909ce9394327be0f9f",
+    "url": "/Workers-List-in-React/static/media/men.a5745666.svg"
+  },
+  {
+    "revision": "6ab43c2455f8f0541c5575337363dd60",
+    "url": "/Workers-List-in-React/static/media/person_search.6ab43c24.svg"
+  },
+  {
+    "revision": "bad2d7d79077610783abd954f07dfbab",
+    "url": "/Workers-List-in-React/static/media/search.bad2d7d7.svg"
+  },
+  {
+    "revision": "3b0f1bc7b38b4791973eab9a99c915c0",
+    "url": "/Workers-List-in-React/static/media/women.3b0f1bc7.svg"
+  }
+]);
